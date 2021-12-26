cfde85566f7eb33be43e4f4d39e8a579663e277d
layne liang <layne.liang@tpv-tech.com>
2021-12-26 11:58:04 +0800

    adjust UI

--- app/src/main/java/com/ex/serialport/MainActivity.java
+++ app/src/main/java/com/ex/serialport/MainActivity.java
@@ -2,6 +2,7 @@ package com.ex.serialport;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
@@ -79,6 +80,11 @@ public class MainActivity extends AppCompatActivity {
         recy.setAdapter(logListAdapter);
         recy.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
 
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
         serialPortFinder = new SerialPortFinder();
         serialHelper = new SerialHelper("dev/ttyS1", 115200) {
             @Override
